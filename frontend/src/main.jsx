ee58b0f98fb7e4dee406a58c5b35ffeb6bd73648
pranavi <pranavigaddam12@gmail.com>
2026-08-25 00:14:16 +0530

    fix: update main.jsx App import and package lock

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './AppRouter.jsx';
+import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AppRouter />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
